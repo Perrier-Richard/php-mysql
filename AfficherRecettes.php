0fd5f26b79975d8bcc2797e9dfa6effd8e2b6250
DidonCode <perrier.richardj@gmail.com>
2023-09-06 10:40:35 +0200

    Les tableaux associatifs

--- AfficherRecettes.php
+++ AfficherRecettes.php
@@ -1,9 +1,17 @@
 <?php
 	$recipes = [
-		['Cassoulet','[...]','mickael.andrieu@exemple.com',true,],
-		['Couscous','[...]','mickael.andrieu@exemple.com',false,],
+		['title'=>'Cassoulet','recipe'=>'Etape 1: faire jsp','author'=>'mickael.andrieu@exemple.com','is_enabled'=> true,],
+		['title'=>'Couscous','recipe'=>'Etape 3: fini','author'=>'mickael.andrieu@exemple.com','is_enabled'=> false,],
+		['title'=>'Escalopemilanaise','recipe'=>'Etape1: regarder top chef','author'=>'mathieu.nebra@exemple.com','is_enabled'=> true,]
 	];
 ?>
+
+<style>
+	h4{
+		padding: 0px;
+		margin: 0px;
+	}
+</style>
 
 <!DOCTYPE html>
 <html>
@@ -11,18 +19,14 @@
 		<title>Affichage des recettes</title>
 	</head>
 	<body>
-		<ul>
-			<?php
-				for($lines = 0; $lines <= 1; $lines++):
-			?>
-			<li>
-				<?php
-					echo $recipes[$lines][0].'('.$recipes[$lines][2].')';
-				?>	
-			</li>
-			<?php
-				endfor;
-			?>
-		</ul>
+		<?php
+			foreach($recipes as $recipe) {
+				if($recipe['is_enabled']){
+					echo "<h2>".$recipe['title']."</h2>";
+					echo "<h4>".$recipe['recipe']."</h4>";
+					echo "<h4>".$recipe['author']."</h4>";
+				}
+			}
+		?>
 	</body>
 </html>
