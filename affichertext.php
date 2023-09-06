0d9eafe5cca6a5a1b21ac6cb249fbc3ad282259c
DidonCode <perrier.richardj@gmail.com>
2023-09-06 10:21:36 +0200

    Ajout de fichier

---
+++ affichertext.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Notre première instruction: echo</title>
+		<meta charset="utf-8"/></head>
+	</head>
+
+	<body>
+		<h2>Affichage de texte en PHP</h2>
+		<p>
+			Cette ligne a été écrite entièrement en HTML.<br>
+			<?php echo "Celle-ci a été écrite entièrement en PHP."; ?>
+		</p>
+	</body>
+</html>
