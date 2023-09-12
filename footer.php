df8b4a37358121621bc424ae4a29126eae11e6ae
DidonCode <perrier.richardj@gmail.com>
2023-09-12 15:51:54 +0200

    Corrigé TP1

--- footer.php
+++ footer.php
@@ -1,1 +1,8 @@
-<p class="bg-light" style="margin: 0 auto; padding: 20px;">Copyright moi, tous droits réservés</p>
+<!-- footer.php -->
+<!-- footer.php -->
+<footer class="bg-light text-center text-lg-start mt-auto">
+  <div class="text-center p-3">
+    © 2021 Copyright:
+    <a class="text-dark">Alexandre Brabant</a>
+  </div>
+</footer>
