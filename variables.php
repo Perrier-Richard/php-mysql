15407e56dfe4968ef9a7dfaba74fefb690c1a195
DidonCode <perrier.richardj@gmail.com>
2023-09-13 10:36:57 +0200

    Ajout d'une clé password dans le tableau  de variables.php

--- variables.php
+++ variables.php
@@ -5,16 +5,19 @@ $users = [
     [
         'full_name' => 'Mickaël Andrieu',
         'email' => 'mickael.andrieu@exemple.com',
+        'password' => 'a',
         'age' => 34,
     ],
     [
         'full_name' => 'Mathieu Nebra',
         'email' => 'mathieu.nebra@exemple.com',
+        'password' => 'b',
         'age' => 34,
     ],
     [
         'full_name' => 'Laurène Castor',
         'email' => 'laurene.castor@exemple.com',
+        'password' => 'c',
         'age' => 28,
     ],
 ];
